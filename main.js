56e2ded6b680a2aa5b9050d1d472ff8e8f456214
Min Theim Kyaw <mintheimkyaw18@gmail.com>
2022-09-25 23:42:08 +0630

    "wrote some functions"

---
+++ main.js
@@ -0,0 +1,19 @@
+const choices = ["rock", "paper", "scissors"]
+
+function game() {
+    //play the game
+    //last five rounds
+    //console based
+}
+function playRound() {
+    const playerSelection = 
+}
+
+function playerChoice() {
+    //get input from the player
+}
+
+function computerChoice() {
+    //get random input from the computer
+    return choices[Math.floor(Math.random()*choices.length)]
+}
