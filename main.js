c0dce7975ba2bc4f691bdc101faf2b378ac9a0a7
Min Theim Kyaw <mintheimkyaw18@gmail.com>
2022-09-27 23:33:00 +0630

    wrote conditional functions for playerChoice

--- main.js
+++ main.js
@@ -13,14 +13,43 @@ function playRound() {
 function playerChoice() {
     //get input from the player
     let input = prompt("plz, type in Rock, Paper or Scissors");
-    input = input.toLowerCase();
 
-    console.log(input);
+    while (input == null){
+        input = prompt("plz, type in Rock, Paper or Scissors"); 
+    }
+
+    input = input.toLowerCase();
+    let check = validateInput(input);
+
+    while (check == false){
+        input = prompt("plz, type in Rock, Paper or Scissors but the capitalization doesn't matter.");
+
+        while (input == null){
+            input = prompt("plz, type in Rock, Paper or Scissors"); 
+        }
+        
+        input = input.toLowerCase();
+        check = validateInput(input);
+    }   
 }
 
 function computerChoice() {
     //get random input from the computer
     return choices[Math.floor(Math.random()*choices.length)];
+}
+
+/*function validateInput(choice){
+    if (choices.includes(choice)){
+        return true;
+    } 
+    return false;
+}*/
+function validateInput(choice){
+    return choices.includes(choice)
+}
+
+function checkWinner(choiceP,choiceC){
+    
 }
 
 game();
